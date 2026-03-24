41de8fbed86cab3d68edbe64f9df47dbcffce4e9
d00ksky <59998532+d00ksky@users.noreply.github.com>
2026-03-24 23:09:12 +0100

    changed for now select discovery papers until we have still sort flag in cli to not have duplication

--- arxiv_app/ranking.py
+++ arxiv_app/ranking.py
@@ -1,8 +1,8 @@
 from arxiv_app.models import Paper
 
 def select_discovery_papers(papers: list[Paper], limit: int = 5) -> list[Paper]:
-    ''' •	preferuj nowsze papers
-	 •	ogranicz liczbę wyników
-	 •	zwróć mały sensowny zestaw'''
-    return sorted(papers, key=lambda paper: paper.year, reverse=True)[:limit]
-    
+    # ''' •	preferuj nowsze papers
+	#  •	ogranicz liczbę wyników
+	#  •	zwróć mały sensowny zestaw'''
+    # return sorted(papers, key=lambda paper: paper.year, reverse=True)[:limit]
+    return papers[:limit]
