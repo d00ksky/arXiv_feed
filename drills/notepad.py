f9a5e9f888b0abf777898ea3fdcf76283cadde0e
d00ksky <debski.jakub@gmail.com>
2026-06-10 22:28:16 +0200

    another one

--- drills/notepad.py
+++ drills/notepad.py
@@ -10,4 +10,14 @@ def titles_after_year(papers: list[dict], year: int) -> list[str]:
     return [paper["title"] for paper in papers if paper["year"] > year]
 
 
-print(titles_after_year(papers, 2019))
+# print(titles_after_year(papers, 2019))
+
+
+def top_titles_by_score(papers: list[dict], limit: int) -> list[str]:
+    return [
+        paper["title"]
+        for paper in sorted(papers, key=lambda paper: paper["score"], reverse=True)
+    ][:limit]
+
+
+print(top_titles_by_score(papers, 3))
