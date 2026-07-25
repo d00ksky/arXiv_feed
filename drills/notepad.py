d57e70d3829e3241a77ddec9647b6cef79d1251b
d00ksky <debski.jakub@gmail.com>
2026-07-25 12:13:40 +0200

    neetcode valid anagram

--- drills/notepad.py
+++ drills/notepad.py
@@ -822,3 +822,15 @@ def hasDuplicate(self, nums: List[int]) -> bool:
                 return True
             seen.add(num)
     return False
+
+# neetcode valid anagram 
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        t_dict = {}
+        for letter in s:
+            s_dict[letter] = s_dict.get(letter, 0) + 1
+        for letter in t:
+            t_dict[letter] = t_dict.get(letter, 0) + 1
+        return s_dict == t_dict
