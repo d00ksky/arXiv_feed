0c70f9c4318c992eeac891f5a4cd7bf597bd7503
d00ksky <debski.jakub@gmail.com>
2026-07-30 23:24:00 +0200

    neetcode: best time to buy and sell stock

--- drills/notepad.py
+++ drills/notepad.py
@@ -921,3 +921,20 @@ assert result == [
     "Retrieval-Augmented Generation",
     "Scaling Language Models",
 ]
+
+# neetcode: best time to buy and sell stock 
+#
+
+def maxProfit(self, prices: List[int]) -> int:
+    
+    profit = 0
+
+    for i in range(len(prices)):
+        price = prices[i]
+        for j in range(i + 1, len(prices)):
+            if prices[j] - price > 0:
+                current_profit = prices[j] - price
+                print(current_profit)
+                if profit < current_profit:
+                    profit = current_profit
+    return profit
