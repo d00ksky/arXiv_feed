274c8d32e07319e11a6719cad5ea2aef1ffc5452
d00ksky <debski.jakub@gmail.com>
2026-08-01 20:22:56 +0200

    neetcode: valid palindrome

--- drills/notepad.py
+++ drills/notepad.py
@@ -956,3 +956,13 @@ def maxProfit(self, prices: List[int]) -> int:
            if profit_new > profit:
                profit = profit_new
    return profit
+
+# neetcode: valid isPalindrome
+
+def isPalindrome(self, s: str) -> bool:
+    cleaned_characters = []
+    for ch in s:
+        if ch.isalnum():
+            cleaned_characters.append(ch.lower())
+    cleaned_string = "".join(cleaned_characters)
+    return cleaned_string == cleaned_string[::-1]
