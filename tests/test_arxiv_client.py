c5dae3cbacf7ad979edede0e3595960c530bb263
d00ksky <debski.jakub@gmail.com>
2026-08-28 23:53:03 +0200

    adding test for new function

--- tests/test_arxiv_client.py
+++ tests/test_arxiv_client.py
@@ -14,3 +14,14 @@ def test_build_query_url():
     assert "sortBy=submittedDate" in url
     assert "sortOrder=descending" in url
     assert "search_query=all:large+language+models" in url
+
+
+
+def test_build_query_url_can_filter_by_category:
+# Ma opisywać zachowanie:
+# _build_query_url("retrieval", 25, category="cs.IR")
+# i sprawdzać, że wynikowe zapytanie zawiera jednocześnie:
+# all:retrieval
+# AND
+# cat:cs.IR
+    pass
