611e8c32df8ba200b0a75cfd22daeaceb7b098cd
d00ksky <debski.jakub@gmail.com>
2026-08-29 15:38:41 +0200

    test for new category flag in arxiv client

--- tests/test_arxiv_client.py
+++ tests/test_arxiv_client.py
@@ -16,12 +16,17 @@ def test_build_query_url():
     assert "search_query=all:large+language+models" in url
 
 
+def test_build_query_url_can_filter_by_category():
+    # Ma opisywać zachowanie:
+    # _build_query_url("retrieval", 25, category="cs.IR")
+    # i sprawdzać, że wynikowe zapytanie zawiera jednocześnie:
+    # all:retrieval
+    # AND
+    # cat:cs.IR
+    query = "retrieval"
+    category = "cs.IR"
 
-def test_build_query_url_can_filter_by_category:
-# Ma opisywać zachowanie:
-# _build_query_url("retrieval", 25, category="cs.IR")
-# i sprawdzać, że wynikowe zapytanie zawiera jednocześnie:
-# all:retrieval
-# AND
-# cat:cs.IR
-    pass
+    url = _build_query_url(query, category)
+
+    assert "all:retrieval" in url
+    assert "cat:cs.IR" in url
